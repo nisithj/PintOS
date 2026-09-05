1552a77cef6f22e9c11d61c4f180a3c1decf62ea
Nisith <nisithjayasuriya@gmail.com>
2026-09-05 14:07:17 +0000

    Implement priority-based thread yielding in thread_set_priority function

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -357,6 +357,12 @@ void
 thread_set_priority (int new_priority) 
 {
   thread_current ()->priority = new_priority;
+
+  if(!list_empty(&ready_list)){
+    if(thread_current ()->priority < list_entry(list_front(&ready_list),struct thread,elem)->priority){
+      thread_yield();
+    }
+  }
 }
 
 /* Returns the current thread's priority. */
