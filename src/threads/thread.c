013952f2f8af29ae7a20d714e06610167c6bf4a4
Nisith <nisithjayasuriya@gmail.com>
2026-09-07 18:50:25 +0000

    Modified init_thread() to match the new fields added to the thread struct

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -504,6 +504,11 @@ init_thread (struct thread *t, const char *name, int priority)
   t->priority = priority;
   t->magic = THREAD_MAGIC;
 
+  /*Priority Donation fields*/
+  t->base_priority = priority;
+  t->waiting_lock = NULL;
+  list_init(&t->donations);
+
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
   intr_set_level (old_level);
