32a0d0f0d6238bf3488ca2705ad933c04ebf671c
Nisith <nisithjayasuriya@gmail.com>
2026-09-07 19:08:52 +0000

    Solved the staleness that comes with priority donantion in ready_list,condition waiters list and semaphore waiters list

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -122,9 +122,11 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters)) 
+  if (!list_empty (&sema->waiters)) {
+    list_sort(&sema->waiters,priority_comparator,NULL);
     thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                 struct thread, elem));
+  }
 
   sema->value++;
   intr_set_level (old_level);
@@ -396,9 +398,11 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
   ASSERT (!intr_context ());
   ASSERT (lock_held_by_current_thread (lock));
 
-  if (!list_empty (&cond->waiters)) 
+  if (!list_empty (&cond->waiters)) {
+    list_sort(&cond->waiters,semaphore_comparator,NULL);
     sema_up (&list_entry (list_pop_front (&cond->waiters),
                           struct semaphore_elem, elem)->semaphore);
+  }
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
--- src/threads/thread.c
+++ src/threads/thread.c
@@ -537,8 +537,10 @@ next_thread_to_run (void)
 {
   if (list_empty (&ready_list))
     return idle_thread;
-  else
+  else {
+    list_sort(&ready_list,priority_comparator,NULL);
     return list_entry (list_pop_front (&ready_list), struct thread, elem);
+  }
 }
 
 /* Completes a thread switch by activating the new thread's page
